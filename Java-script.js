263a83cf51a79cfb3a9768857e7e3b2e92c6e516
Gohil Kaushik <93031942+gohilkaushik@users.noreply.github.com>
2022-08-10 17:50:11 +0530

    Add files via upload

---
+++ Java-script.js
@@ -0,0 +1,16 @@
+const hamburgerBtn = document.querySelector(".hamburger-btn");
+
+hamburgerBtn.onclick = () => {
+  const headerRight = document.querySelector(".header-right");
+  const bodyOverlay = document.querySelector("body");
+  const menuIcon = document.querySelector(".menu-icon");
+
+  headerRight.classList.toggle("open");
+  bodyOverlay.classList.toggle("overlay");
+  menuIcon.classList.toggle("on");
+  if (menuIcon.classList.contains("on")) {
+    menuIcon.src = "./images/close.png";
+  } else {
+    menuIcon.src = "./images/menu.png";
+  }
+};
