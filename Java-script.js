75784be22662f8d4e23721c7ba90fbea8a055d2c
Gohil Kaushik <93031942+gohilkaushik@users.noreply.github.com>
2023-01-15 21:25:15 +0530

    Update Java-script.js

--- Java-script.js
+++ Java-script.js
@@ -16,3 +16,31 @@ hamburgerBtn.onclick = () => {
   }
 };
 // End
+
+
+// hamburgerbtn code 
+const navToggle = document.querySelector(".navmenu");
+const menuItems = document.querySelectorAll(".menu-item-has-children");
+const subMenu = document.querySelectorAll(".sub-menu");
+const menu = document.querySelectorAll(".ok");
+
+document.querySelector(".navbartoggle").addEventListener("click", (e) => {
+    navToggle.classList.add("slide");
+});
+
+document.querySelector(".headerclose").addEventListener("click", (e) => {
+    navToggle.classList.remove("slide");
+});
+
+menuItems.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+        const check = ele.querySelector("ul").classList.contains("ok");
+        check == true ? closeSubmenu() : closeSubmenu() || ele.querySelector("ul").classList.add("ok");
+    })
+})
+
+function closeSubmenu() {
+    subMenu.forEach((ele) => {
+        ele.classList.remove("ok");
+    });
+}
